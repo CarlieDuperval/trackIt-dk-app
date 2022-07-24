4854016cc6b87e6b45c5f730a95455f1ed90ad5f
Carlie Duperval <ritchellcarlie@gmail.com>
2022-07-24 16:10:47 -0400

    Created data base on connectDb file

--- functions/src/gateway/connectDb.js
+++ functions/src/gateway/connectDb.js
@@ -2,3 +2,13 @@ import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+export const getDb = async () => {
+    const client = new MongoClient(process.env.MONGO_URL)
+    await client.connect()
+    return client.db("TrackIt-Dk")
+}
+
+export const getSalesCollection = async () => {
+    const db = await getDb
+}
