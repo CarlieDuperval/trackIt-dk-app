51aadfc038acdb9c298b3e26f6e8694feb6d67c9
Carlie Duperval <ritchellcarlie@gmail.com>
2022-07-27 10:19:06 -0400

    Added sale Object

--- functions/src/routes/sales-routes.js
+++ functions/src/routes/sales-routes.js
@@ -9,8 +9,19 @@ import {
 export const salesRouter = Router()
 
 salesRouter.post('sales', async (req, res)=>{
-    const { year, productName, productCategory, qtySold} = req.body
+    const { year, productName, productCategory, price, qtySold} = req.body
     if(!year|| !productName|| !productCategory){
         res.status(401).send('Invalid Request')
     }
+
+    const sale ={
+        year,
+        productCode,
+        productDescription,
+        productCategory,
+        price,
+        qtySold
+    }
+    const result = await createSale(sale)
+    res.status(201).send(result)
 })
